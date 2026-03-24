02321ca399bcaf45cc0ef2a0b5664bc3f9987086
Zoraiz Bin Zahid <zoraizbinzahid@gmail.com>
2026-03-24 16:15:23 +0500

    fixed environment variables

---
+++ dist/static/sanity-DP6nlSpt.js
@@ -0,0 +1,1 @@
+import{r as t,s as e}from"./sanity.config-CcJ3tYc4.js";t(document.getElementById("sanity"),e,{reactStrictMode:!1,basePath:"/"});
--- sanity/env.ts
+++ sanity/env.ts
@@ -1,20 +1,6 @@
 export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2026-03-10'
 
-export const dataset = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_DATASET,
-  'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET'
-)
+export const dataset = 'production'
 
-export const projectId = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
-)
-
-function assertValue<T>(v: T | undefined, errorMessage: string): T {
-  if (v === undefined) {
-    throw new Error(errorMessage)
-  }
-
-  return v
-}
+export const projectId = 'g35w6nr8'
